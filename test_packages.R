e66e3f2195ec8c8531a294505031f1cd7b745d98
MCabreraBrufau <mcabrerabrufau@gmail.com>
2024-04-19 20:52:34 +0200

    Additions to script

--- test_packages.R
+++ test_packages.R
@@ -13,30 +13,91 @@ csvfolder <- "C:/Users/Miguel/Desktop/GC_new/GCexportedfiles/"
 filelist<- list.files(path = csvfolder, full.names = F)
 
 
-#Following in this script are test to process these 22 types of data with different packages.
+#Following in this script are test to process these 2 types of data with different packages. I will try the following packages: 
 
-#some are written in python, which requires the package reticulate to be run in R as an interface
-install.packages('reticulate')
+# RpeakChrom
+# chromatographR
+# xcms
 
-library(tidyverse)
 
-#####1_RpeakChrom pkg####
 
-# install.packages('RpeakChrom')
+#####1. RpeakChrom pkg####
 
-library(RpeakChrom)
+# 
+if(!require(RpeakChrom)){
+  install.packages('RpeakChrom')
+}
+
 
 test<-read.csv("C:/Users/Miguel/Desktop/GC_new/chromatograma ejemplo.csv")
 str(test)
 
-testch<-RpeakChrom::readChrom(filepath = "C:/Users/Miguel/Desktop/GC_new/chromatograma ejemplo.csv", do.plot = F, t1=10000, t2=50000)
+######1.1 import######
+testa<-RpeakChrom::readChrom(filepath = "C:/Users/Miguel/Desktop/GC_new/chromatograma ejemplo.csv", do.plot = T, t1=20, t2=500)
+#Import function does not allow for headers in csv... causes all data to be imported as character
+str(testa)
 
-str(testch)
-str(test)
 
-ggplot(test, aes(x=RT.milliseconds., y=TIC))+geom_line()
 
-RpeakChrom::processPeak(peak = testch,baseline = T,method ='pvmg' ,compound = 's',flow = 5,area = T)
+maxRT<-1539552
+#try using an example file without headers
+testb<-RpeakChrom::readChrom(filepath = "C:/Users/Miguel/Desktop/GC_new/chromatograma ejemplo_noheader.csv", do.plot = T, t1=1.65e5, t2=1.85e5)
+
+str(testb)
+#removing headers works
+
+
+max(testb$V2)
+
+processPeak(peak = testb,baseline = T,method ='direct',area = T, compound = 'text', 
+flow = 'text2')
+#something fails with the integration/modeling of the peak
+
+
+parameters <- processPeak(peak, baseline = FALSE, flow = 0.1, method = "pvmg",
+                          compound = "alanine", area = TRUE)
+
 
 install.packages("chromConverter")
+plot(peak)
+plot(testb)
+peak
+
+####2. chromatographR pkg####
+
+# chromatographR is a package for the reproducible analysis of HPLC-DAD chromatographic data in R. It can also be used to analyze other "simple" chromatographic data like GC-FID, HPLC-UV, or HPLC-FD.
+
+#Install:
+if(!require(chromatographR)){
+  if(!require(remotes)){
+    install.packages("remotes") 
+  }
+  remotes::install_github("https://github.com/ethanbass/chromatographR/")
+}
+
+
+
+
+
+
+
+
+
+
+####3. xcmx pkg####
+
+#the xcmx package belongs to the bioconductor universe, it was developed to work with LC-MS and GC-MS data, but we can make use of its functionalities for basic chromatographic processinga
+#install:
+
+if (!require("xcms", quietly = F)){
+  if (!require("BiocManager", quietly = F)){
+    install.packages("BiocManager")
+  }
+  BiocManager::install("xcms")
+}
+
+require(xcms)
+
+
+
 
