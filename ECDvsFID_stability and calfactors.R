8813dd24641b8016f33ec6e114af35578c86b3db
Miguel Cabrera Brufau <mcabrerabrufau@gmail.com>
2024-06-18 18:25:37 +0200

    Data expanded, calcurves calculated and plots made for quality check of ECD and FID detectors

--- ECDvsFID_stability and calfactors.R
+++ ECDvsFID_stability and calfactors.R
@@ -11,16 +11,32 @@ library(broom)
 library(ggpmisc)
 library(egg)
 
-
+#Directories for data and plots
 dropbox_root<- "C:/Users/Miguel/Dropbox/"
 gcfolder<- paste0(dropbox_root,"RESTORE4Cs - Fieldwork/Data/GHG/GC data/")
 plots_path<- paste0(dropbox_root,"GC data/plots/")
 
 
-gcog<- read_xlsx(path = paste0(gcfolder,"DATA_R4Cs_formatted_ok_v20240615.xlsx"),range = cell_cols("A:I"))
-names(gcog)
+
+# ---- function to save a list of plots into pdf file ----
+gg_save_pdf = function(list, filename) {
+  pdf(filename,width = 14,height = 7)
+  for (p in list) {
+    print(p)
+  }
+  dev.off()
+  invisible(NULL)
+}
+
+
+
+#Data import and formatting
+
+gcog<- read_xlsx(path = paste0(gcfolder,"DATA_R4Cs_formatted_ok_v20240618.xlsx"),range = cell_cols("A:I"))
 names(gcog)<- c("sampleid","seq_pos","date","batch","fileid","ch4","co2","n2o","obs")
 
+
+#Select only standard mix injections and formatting
 gc_work<- gcog %>% 
   select(sampleid, seq_pos,date,batch,ch4,co2,n2o,obs) %>% 
   mutate(datef=as.Date.character(as.character(date),format="%Y%m%d")) %>% 
@@ -31,11 +47,11 @@ gc_work<- gcog %>%
   mutate(standardgas=sub("\\_.*", "", sampleid),#Extract PXX from sampleid
          vol= case_when(standardgas=="P20"~11.2,
                        TRUE~as.numeric(sub("P","", standardgas)))) %>% #Extract standard volume gas from PXX
-  mutate(uniqueid=paste(rownames(.),standardgas,date,batch,sep = "_"))#add unique id with explicit info
+  mutate(uniqueinj_id=paste(rownames(.),date,batch,seq_pos,sep="_"))#add unique id with explicit info
 
 
 #Check uniqueid is in fact unique
-dim(gc_work)[1]==length(unique(gc_work$uniqueid))
+dim(gc_work)[1]==length(unique(gc_work$uniqueinj_id))
 
 #Check what dates have a complete cal curve integrated
 dates_with_calcurve<- gc_work %>% 
@@ -48,12 +64,10 @@ dates_with_calcurve<- gc_work %>%
 #Subset dates with complete cal curve
 gc_complete<- gc_work %>% 
   filter(date%in%dates_with_calcurve) %>% 
-  filter(date!=20240614&date!=20240615)
+  filter(date<20240614)#keep only dates before overnight ECD running test
 
 
-
-
-
+#example plot of calcurve
 gc_complete%>%
   filter(date==20240410) %>% 
   filter(batch==0) %>% 
@@ -62,28 +76,39 @@ gc_complete%>%
   stat_poly_line() +
   stat_poly_eq(use_label(c("eq", "adj.R2", "n")))
 
+#Pivot longer for calculations and add uniqueareaid
+gc_complete_long <- gc_complete %>% 
+  pivot_longer(cols=c("co2","ch4","n2o"), names_to = "gas", values_to = "area") %>% 
+  mutate(uniqueareaid=paste(date,batch,seq_pos,gas,sep="_"))
 
 
 
+#Outliers uniqueareaid: filled in manually after inspection of individual calplots 
+co2_out<- c("20240402_0_3_co2","20240404_0_3_co2","20240415_0_3_co2")
+ch4_out<- c("20240402_0_3_ch4","20240405_0_7_ch4","20240410_0_3_ch4","20240415_0_3_ch4")
+n2o_out<- c("20240321_0_5_n2o","20240404_0_5_n2o","20240506_0_3_n2o")
 
-#Calculate all the cal curves
-cal<- gc_complete %>%
+
+
+#Calculate all the cal curves parameters
+cal<- gc_complete_long %>%
   filter(batch==0) %>% 
-  # filter(vol!=11.2) %>% #NO P20s!
-  # filter(vol!=0) %>%  #NO blanks!
-  select(uniqueid,date,vol,co2,ch4,n2o)%>%
-  pivot_longer(cols = c(co2,ch4,n2o), names_to = "gas",values_to = "area") %>% 
+  filter(vol!=11.2) %>% #NO P20s!
+  filter(vol!=0) %>%  #NO blanks!
+  filter(!(uniqueareaid)%in%c(co2_out,ch4_out,n2o_out)) %>% 
+  select(uniqueinj_id,uniqueareaid,date,vol,gas,area)%>%
   nest_by(date,gas) %>% 
   mutate(mod = list(lm(area~vol, data=data))) %>%
   reframe(tidy(mod),glance(mod))
-  
+
   
 #Plot factors 
 cal%>%
   filter(term=="vol")%>%
   # filter(r.squared>0.95) %>% 
 ggplot(aes(x=as.Date(as.character(date),format="%Y%m%d"), y=estimate, col=gas))+
-         geom_point()
+         geom_point()+
+  facet_wrap(facets=.~gas, scales = "free")
   
 ##Plot R2
 cal%>%
@@ -92,193 +117,195 @@ cal%>%
   geom_point()
 
  
-#We need to inspect all the calibration curves one by one!
+#We need to inspect all the calibration curves one by one! DONE, (outliers annotated in gas_out vectors)
  
+#PLot every calcurve (after excluding outliers explictly, specified in "gas_out" vectors)
+
+#Remove objects created in loop (for repeated executions of loop) 
+rm(excluded,calcurve,plt_CH4,plt_CO2,plt_N2O,datecalcurve)
+
+#create vector to store calcurve plots
+plt_list <- vector('list', length(unique(gc_complete$date)))
+
+#Loop over each date and plot the calcurve for all 3 ghgs
+for(datecalcurve in unique(gc_complete$date)){
+  
+  calcurve<- gc_complete_long %>% 
+    filter(date==datecalcurve) %>% 
+    filter(batch==0) %>% 
+    filter(vol!=11.2) %>% 
+    filter(vol!=0) %>% 
+    filter(!(uniqueareaid)%in%c(co2_out,ch4_out,n2o_out))
+  
+  excluded<- gc_complete_long %>% 
+    filter(date==datecalcurve) %>% 
+    filter(batch==0) %>% 
+    filter(vol!=11.2) %>% 
+    filter(vol!=0) %>% 
+    filter(!(uniqueareaid%in%calcurve$uniqueareaid))
+  
+  
+  plt_CO2 <- ggplot(subset(calcurve, gas=="co2"), aes(vol, area))+
+    geom_point()+
+    # geom_text(aes(label=uniqueareaid))+
+    stat_poly_line() +
+    stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
+    geom_point(data = subset(excluded, gas=="co2"), aes(vol,area, col="excluded"))+
+    # geom_text(data=subset(qual_inj, gas=="co2"), aes(vol, area,label=uniqueareaid))+
+    theme_article()+
+    labs(col = "") +
+    theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
+    xlab("Std gas vol (ml)")+
+    ylab("CO2 Area (FID)")+
+    ggtitle(paste0(datecalcurve," cal. plots"))
+  
+  plt_CH4 <- ggplot(subset(calcurve, gas=="ch4"), aes(vol, area))+
+    geom_point()+
+    # geom_text(aes(label=uniqueareaid))+
+    stat_poly_line() +
+    stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
+    geom_point(data = subset(excluded, gas=="ch4"), aes(vol,area, col="excluded"))+
+    # geom_text(data=subset(qual_inj, gas=="ch4"), aes(vol, area,label=uniqueareaid))+
+    theme_article()+
+    labs(col = "") +
+    theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
+    xlab("Std gas vol (ml)")+
+    ylab("CH4 Area (FID)")
+  
+  plt_N2O <- ggplot(subset(calcurve, gas=="n2o"), aes(vol, area))+
+    geom_point()+
+    # geom_text(aes(label=uniqueareaid))+
+    stat_poly_line() +
+    stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
+    geom_point(data = subset(excluded, gas=="n2o"), aes(vol,area, col="excluded"))+
+    # geom_text(data=subset(qual_inj, gas=="n2o"), aes(vol, area,label=uniqueareaid))+
+    theme_article()+
+    labs(col = "") +
+    theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
+    xlab("Std gas vol (ml)")+
+    ylab("N2O Area (ECD)")
+  
+  # plt <- ggarrange(plt_CO2, plt_CH4, plt_H2O, ncol = 1)
+  plt_list[[which(unique(gc_complete$date)==datecalcurve)]] <- ggarrange(plt_CO2, plt_CH4, plt_N2O, ncol = 3,nrow = 1)
+  
+}
+
+# Print pdf
+setwd(plots_path)
+gg_save_pdf(list = plt_list, filename = paste0("calcurves.pdf"))
+
+
+rm(excluded,calcurve,plt_CH4,plt_CO2,plt_N2O,datecalcurve,plt_list)
+
+
+
+
+#Calculate relative deviation of each Pinjection with respect to the daily calfactor
+
+#Do all data in gc_complete_long have a calfactor?
+unique(unique(gc_complete_long$date)%in%unique(cal$date))
+
+date_factor<- cal %>% 
+  filter(term=="vol") %>% 
+  select(date, gas, estimate) %>% 
+  rename(factor=estimate)
+
+gc_complete_long_cal<- gc_complete_long %>% 
+  merge.data.frame(date_factor, by=c("date","gas"), all = T) %>% 
+  mutate(vol_est=area/factor)
+
+
+
+
+#Timeseries of factor stability for each gas 
+cal%>%
+  filter(term=="vol")%>%
+  group_by(gas) %>% 
+  mutate(average_factor=mean(estimate)) %>% 
+  # filter(r.squared>0.95) %>% 
+  ggplot(aes(x=as.Date(as.character(date),format="%Y%m%d"), y=estimate/average_factor, col=gas))+
+  geom_point()+
+  facet_grid(rows = vars(gas))
+
+#PLot of relative calibration factor stability
+cal%>%
+  filter(term=="vol")%>%
+  group_by(gas) %>% 
+  mutate(average_factor=mean(estimate)) %>% 
+  # filter(r.squared>0.95) %>% 
+  ggplot(aes(x=gas, y=estimate/average_factor, fill=gas))+
+  geom_violin()+
+  geom_hline(yintercept = 1)+
+  geom_dotplot(binaxis= "y",
+               stackdir = "center",binwidth=0.01,
+               # dotsize = 1,
+               fill = 1) +
+  ggtitle("Relative deviation of daily calibration factor")
+
+
 
 # PLOT relative deviation of P5 areas for each of the gasses
-gc_work %>% 
-  filter(date!=20240614&date!=20240615) %>% #Exclude last days
-  filter(vol==5) %>% 
-  pivot_longer(cols = c(co2,ch4,n2o), names_to = "gas",values_to = "area") %>% 
-  filter(!is.na(area)) %>% 
-  group_by(gas) %>% 
-  mutate(meanP5area=mean(area)) %>% 
-  ggplot(aes(x=gas, y=area/meanP5area, col=gas))+geom_violin()+
-  geom_jitter(width = 0.2,size = 1)
-
-
-
-# ---- function to save a list of plots into pdf file ----
-
-# gg_save_pdf = function(list, filename) {
-#   pdf(filename,width = 14,height = 7)
-#   for (p in list) {
-#     print(p)
-#   }
-#   dev.off()
-#   invisible(NULL)
-# }
-# 
-# plt_list <- vector('list', length(unique(gc_complete$date)))
-# 
-# 
-# 
-# for(datecalcurve in unique(gc_complete$date)){
-#   
-#   calcurve<- gc_complete %>% filter(date==datecalcurve) %>% 
-#     filter(batch==0) %>% 
-#     filter(vol!=11.2) %>% 
-#     filter(vol!=0)
-# 
-#   qual_inj<- gc_complete %>% 
-#     filter(date==datecalcurve) %>% 
-#     filter(!(uniqueid%in%calcurve$uniqueid))
-#   
-#   plt_CO2 <- ggplot(calcurve, aes(vol, co2))+
-#     geom_point()+
-#     stat_poly_line() +
-#     stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
-#     geom_point(data = qual_inj, aes(vol,co2, col="excluded"))+
-#     theme_article()+
-#     labs(col = "") +
-#     theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
-#     xlab("Std gas vol (ml)")+
-#     ylab("CO2 Area (FID)")+
-#     ggtitle(paste0(datecalcurve," cal. plots"))
-#             
-#   plt_CH4 <- ggplot(calcurve, aes(vol, ch4))+
-#     geom_point()+
-#     stat_poly_line() +
-#     stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
-#     geom_point(data = qual_inj, aes(vol,ch4, col="excluded"))+
-#   theme_article()+
-#     labs(col = "") +
-#     theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
-#     xlab("Std gas vol (ml)")+
-#     ylab("CH4 Area (FID)")
-#   
-#   plt_N2O <- ggplot(calcurve, aes(vol, n2o))+
-#     geom_point()+
-#     stat_poly_line() +
-#     stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
-#     geom_point(data = qual_inj, aes(vol,n2o, col="excluded"))+
-#   theme_article()+
-#     labs(col = "") +
-#     theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
-#     xlab("Std gas vol (ml)")+
-#     ylab("N2O Area (ECD)")
-#   
-#   # plt <- ggarrange(plt_CO2, plt_CH4, plt_H2O, ncol = 1)
-#   plt_list[[which(unique(gc_complete$date)==datecalcurve)]] <- ggarrange(plt_CO2, plt_CH4, plt_N2O, ncol = 3,nrow = 1)
-#   
-# }
-# 
-#   # Print pdf
-#   setwd(plots_path)
-#   gg_save_pdf(list = plt_list, filename = paste0("calcurves.pdf"))
-# 
-#   
-#   
-#   
-#   
-#   
-#   gg_save_pdf = function(list, filename) {
-#     pdf(filename,width = 14,height = 7)
-#     for (p in list) {
-#       print(p)
-#     }
-#     dev.off()
-#     invisible(NULL)
-#   }
-#   
-#   plt_list <- vector('list', length(unique(gc_complete$date)))
-#   
-rm(qual_inj,calcurve,plt_CH4,plt_CO2,plt_N2O,datecalcurve)
-  gc_complete_long <- gc_complete %>% 
-    pivot_longer(cols=c("co2","ch4","n2o"), names_to = "gas", values_to = "area") %>% 
-    mutate(uniqueareaid=paste(date,batch,seq_pos,gas,sep="_"))
-  
-  #Outliers uniqueareaid:
-  co2_out<- c("20240402_0_3_co2","20240404_0_3_co2","20240415_0_3_co2")
-  ch4_out<- c("20240402_0_3_ch4","20240405_0_7_ch4","20240410_0_3_ch4","20240415_0_3_ch4")
-  n2o_out<- c("20240321_0_5_n2o","20240404_0_5_n2o","20240506_0_3_n2o")
-  
-  plt_list <- vector('list', length(unique(gc_complete$date)))
-  
-  gg_save_pdf = function(list, filename) {
-        pdf(filename,width = 14,height = 7)
-        for (p in list) {
-          print(p)
-        }
-        dev.off()
-        invisible(NULL)
-      }
-  
-  
-  
-  for(datecalcurve in unique(gc_complete$date)){
-    
-    calcurve<- gc_complete_long %>% filter(date==datecalcurve) %>% 
-      filter(batch==0) %>% 
-      filter(vol!=11.2) %>% 
-      filter(vol!=0) %>% 
-      filter(!(uniqueareaid)%in%c(co2_out,ch4_out,n2o_out))
-    
-    qual_inj<- gc_complete_long %>% 
-      filter(date==datecalcurve) %>% 
-      filter(!(uniqueareaid%in%calcurve$uniqueareaid))
-    
-    
-    plt_CO2 <- ggplot(subset(calcurve, gas=="co2"), aes(vol, area))+
-      geom_point()+
-      geom_text(aes(label=uniqueareaid))+
-      stat_poly_line() +
-      stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
-      geom_point(data = subset(qual_inj, gas=="co2"), aes(vol,area, col="excluded"))+
-      geom_text(data=subset(qual_inj, gas=="co2"), aes(vol, area,label=uniqueareaid))+
-      theme_article()+
-      labs(col = "") +
-      theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
-      xlab("Std gas vol (ml)")+
-      ylab("CO2 Area (FID)")+
-      ggtitle(paste0(datecalcurve," cal. plots"))
-    
-    plt_CH4 <- ggplot(subset(calcurve, gas=="ch4"), aes(vol, area))+
-      geom_point()+
-      geom_text(aes(label=uniqueareaid))+
-      stat_poly_line() +
-      stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
-      geom_point(data = subset(qual_inj, gas=="ch4"), aes(vol,area, col="excluded"))+
-      geom_text(data=subset(qual_inj, gas=="ch4"), aes(vol, area,label=uniqueareaid))+
-      theme_article()+
-      labs(col = "") +
-      theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
-      xlab("Std gas vol (ml)")+
-      ylab("CH4 Area (FID)")
-    
-    plt_N2O <- ggplot(subset(calcurve, gas=="n2o"), aes(vol, area))+
-      geom_point()+
-      geom_text(aes(label=uniqueareaid))+
-      stat_poly_line() +
-      stat_poly_eq(use_label(c("eq", "adj.R2", "n")))+
-      geom_point(data = subset(qual_inj, gas=="n2o"), aes(vol,area, col="excluded"))+
-      geom_text(data=subset(qual_inj, gas=="n2o"), aes(vol, area,label=uniqueareaid))+
-      theme_article()+
-      labs(col = "") +
-      theme(legend.position = "inside", legend.position.inside = c(0.8,0.1))+
-      xlab("Std gas vol (ml)")+
-      ylab("N2O Area (ECD)")
-    
-    # plt <- ggarrange(plt_CO2, plt_CH4, plt_H2O, ncol = 1)
-    plt_list[[which(unique(gc_complete$date)==datecalcurve)]] <- ggarrange(plt_CO2, plt_CH4, plt_N2O, ncol = 3,nrow = 1)
-    
-  }
-  
+#Comparison of GHG P5 injection quality
+gc_complete_long_cal %>% 
+  filter(vol==5) %>%
+  ggplot(aes(x=gas, y=vol_est, fill=gas))+geom_violin()+
+  # geom_jitter(width = 0.2,size = 1)+
+  geom_hline(yintercept = 5)+
+  geom_dotplot(binaxis= "y",
+               stackdir = "center",binwidth=0.25,
+               dotsize = 0.5,
+               fill = 1) +
+  scale_y_continuous(name="Estimated volume of std gas (ml)")+
+  ggtitle("P5 quality-check injections estimated vol")
 
   
-  # Print pdf
-  setwd(plots_path)
-  gg_save_pdf(list = plt_list, filename = paste0("calcurves.pdf"))
+#Timeseries of measured P5 volume of gas
+gc_complete_long_cal %>% 
+  filter(vol==5) %>%
+  ggplot(aes(x=datef, y=vol_est, col=gas, group=datef))+
+  geom_hline(yintercept = 5)+
+  # geom_boxplot()+
+  geom_jitter(size=0.8, width=0.2)+
+  scale_x_date(name="Date of analysis", date_breaks = "1 month", date_minor_breaks = "7 days",date_labels = "%B")+
+  scale_y_continuous(name="Estimated volume of std gas (ml)")+
+  facet_grid(rows = vars(gas))+
+  ggtitle("P5 deviation from daily calibration curve")
+
+#Timeseries of relative deviation of P5 measured vs expected volume
+gc_complete_long_cal %>% 
+  filter(vol==5) %>%
+  ggplot(aes(x=datef, y=(vol_est/5)-1, col=gas, group=datef))+
+  geom_hline(yintercept = 0)+
+  # geom_boxplot()+
+  geom_jitter(size=0.8, width=0.2)+
+  scale_x_date(name="Date of analysis", date_breaks = "1 month", date_minor_breaks = "7 days",date_labels = "%B")+
+  scale_y_continuous(name="Relative deviation from expected value")+
+  facet_grid(rows = vars(gas))+
+  ggtitle("P5 relative deviation from daily calibration curve")
+
   
+
+#Expected vs measured volume across all days for the 3 GHG (scatter +jitter)
+gc_complete_long_cal %>% 
+  ggplot(aes(x=vol, y=vol_est,col=gas))+
+  geom_abline(intercept = 0, slope = 1, linewidth=1)+
+  geom_jitter(size=0.8, width=0.2)+
+  facet_grid(cols=vars(gas))+
+  theme_bw()+
+  scale_y_continuous(name="Measured vol (ml)", breaks = c(0,1,2,4,6,8,10,12, 15,20,25))+
+  scale_x_continuous(name="Actual vol injected (ml), with 0.2ml jitter", breaks = c(0,1,2,4,6,8,10,12))+
+  ggtitle("Known vs measured volume in all standard gas injections")
   
-  
+
+
+#Expected vs measured volume across all days for the 3 GHG (boxplot)
+gc_complete_long_cal %>% 
+  filter(vol!=3) %>% 
+  ggplot(aes(x=vol, y=vol_est,col=gas, group=vol))+
+  geom_boxplot()+
+  geom_abline(intercept = 0, slope = 1)+
+  facet_grid(cols=vars(gas))+
+  scale_y_continuous(name="Measured vol (ml)", breaks = c(0,1,2,4,6,8,10,12, 15,20,25))+
+  scale_x_continuous(name="Actual vol injected (ml), with 0.2ml jitter", breaks = c(0,1,2,4,6,8,10,12))+
+  ggtitle("Known vs measured volume in all standard gas injections")
+
